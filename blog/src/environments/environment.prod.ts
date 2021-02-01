158db7f1e69a42c941c33e1663024bfbe63a49d5
Ghoust48 <slepuhin.ghoust48@gmail.com>
2021-02-01 19:12:41 +0300

    Updated environments and added environment interface

--- blog/src/environments/environment.prod.ts
+++ blog/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from "./environment.interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBxbVYkpQitv6D_ZYZYsfXRSNFAnHQ9D7Y'
 };
