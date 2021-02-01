65bb7cadd70f0aaa4d0c5c7c836f852a6ef43ea4
Ghoust48 <slepuhin.ghoust48@gmail.com>
2021-02-01 19:11:18 +0300

    Implemented login page components

--- blog/src/app/admin/login-page/login-page.component.ts
+++ blog/src/app/admin/login-page/login-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {User} from "../../shared/interfaces/user";
+import {AuthService} from "../shared/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  private _form: FormGroup;
 
-  ngOnInit(): void {
+  private _isSubmitted: boolean;
+
+  constructor(public authService: AuthService, private _router: Router) { }
+
+  public get form(): FormGroup {
+    return this._form;
   }
 
+  public set form(value: FormGroup) {
+    this._form = value;
+  }
+
+  get isSubmitted(): boolean {
+    return this._isSubmitted;
+  }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ]),
+    })
+  }
+
+  public submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this._isSubmitted = true;
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
+
+    this.authService.login(user).subscribe(() => {
+      this.form.reset();
+      this._router.navigate(['/admin', 'dashboard']);
+      this._isSubmitted = false;
+    }, error => {
+      this._isSubmitted = false;
+    });
+  }
 }
